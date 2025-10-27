35ba9d79da10d8f153f147860f1e0719724a3ec6
eMD0gG <vaspyrkov2@gmail.com>
2025-10-27 15:36:33 +1000

    add csv directory path into services yaml
    add booking API and csv service

---
+++ src/Controller/HouseController.php
@@ -0,0 +1,77 @@
+<?php
+namespace App\Controller;
+
+use App\Service\CsvService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api/houses')]
+class HouseController extends AbstractController
+{
+    private CsvService $csvService;
+
+    public function __construct(CsvService $csvService)
+    {
+        $this->csvService = $csvService;
+    }
+
+    #[Route('/available', methods: ['GET'])]
+    public function getAvailable(): JsonResponse
+    {
+        $houses = $this->csvService->readCsv('houses.csv');
+        $bookings = $this->csvService->readCsv('bookings.csv');
+
+        $bookedIds = array_column($bookings, 'house_id');
+
+        $available = array_filter($houses, function ($house) use ($bookedIds) {
+        return !in_array($house['id'], $bookedIds);
+        });
+
+        return new JsonResponse($available);
+    }
+
+    #[Route('/book', methods: ['POST'])]
+    public function book(Request $request): JsonResponse
+    {
+        $bookings = $this->csvService->readCsv('bookings.csv');
+        $data = json_decode($request->getContent(), true);
+
+        $newBooking = [
+            'id' => count($bookings) + 1,
+            'house_id' => $data['cottage_id'],
+            'phone' => $data['phone'],
+            'comment' => $data['comment'] ?? ''
+        ];
+
+        $bookings[] = $newBooking;
+        $this->csvService->writeCsv('bookings.csv', $bookings, ['id', 'cottage_id', 'phone', 'comment']);
+
+        return new JsonResponse(['status' => 'success', 'booking' => $newBooking]);
+    }
+
+    #[Route('/book/{id}', methods: ['PUT'])]
+    public function updateBooking(int $id, Request $request): JsonResponse
+    {
+        $bookings = $this->csvService->readCsv('bookings.csv');
+        $data = json_decode($request->getContent(), true);
+        $updated = false;
+
+        foreach ($bookings as &$booking) {
+            if ($booking['id'] == $id) {
+                $booking['comment'] = $data['comment'] ?? '';
+                $updated = true;
+                break;
+            }
+        }
+
+        if ($updated) {
+            $this->csvService->writeCsv('bookings.csv', $bookings, ['id', 'cottage_id', 'phone', 'comment']);
+            return new JsonResponse(['status' => 'updated']);
+        }
+
+        return new JsonResponse(['status' => 'not found'], 404);
+    }
+
+}
