61fe1ab0c86cd2e3207428ebb50227612482077b
eMD0gG <vaspyrkov2@gmail.com>
2025-12-04 20:31:30 +1000

    fix

--- src/Controller/Admin/BookingRequestCrudController.php
+++ src/Controller/Admin/BookingRequestCrudController.php
@@ -7,6 +7,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 
 class BookingRequestCrudController extends AbstractCrudController
 {
@@ -15,14 +17,19 @@ class BookingRequestCrudController extends AbstractCrudController
         return BookingRequest::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('comment', 'Комментарий'),
+            AssociationField::new('user', 'Пользователь'),
+            AssociationField::new('house', 'Дом'),
+            ChoiceField::new('status', 'Статус')
+                ->setChoices([
+                    'Ожидает' => 'pending',
+                    'Подтверждено' => 'confirmed',
+                    'Отменено' => 'cancelled',
+                ]),
         ];
     }
-    */
 }
